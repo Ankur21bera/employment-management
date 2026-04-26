662c7fe96e171e2cf20404b56118610e2e93ab6a
Ankur Bera <ankurbera07@gmail.com>
2026-04-26 21:35:31 +0530

    updated payslip routing fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL:(import.meta.VITE_BASE_URL || "http://localhost:4000") + "/api"
+    baseURL:(import.meta.VITE_BASE_URL || "https://employment-backend.vercel.app") + "/api"
 })
 
 api.interceptors.request.use((config)=>{
